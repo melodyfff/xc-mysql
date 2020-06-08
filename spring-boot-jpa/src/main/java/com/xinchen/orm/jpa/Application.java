027d338d19316313436de9879a56ae0044724400
melodyfff <307208327@qq.com>
2020-06-08 17:30:46 +0800

    :rocket: springboot jpa batch insert/update

--- spring-boot-jpa/src/main/java/com/xinchen/orm/jpa/Application.java
+++ spring-boot-jpa/src/main/java/com/xinchen/orm/jpa/Application.java
@@ -3,11 +3,21 @@ package com.xinchen.orm.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+/**
+ *
+ * springboot-jpa
+ *
+ * Batch insert/update reference: https://www.baeldung.com/jpa-hibernate-batch-insert-update
+ *
+ */
 @SpringBootApplication
 @EntityScan("com.xinchen.orm.jpa.core.entity")
-public class SpringBootJpaApplication {
+@EnableJpaRepositories
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- spring-boot-jpa/src/main/java/com/xinchen/orm/jpa/run/Runner.java
+++ spring-boot-jpa/src/main/java/com/xinchen/orm/jpa/run/Runner.java
@@ -5,14 +5,16 @@ import com.xinchen.orm.jpa.core.repositories.BaseDataRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 /**
- *
  * jpa批量插入实验
  *
  * @author xinchen
@@ -22,22 +24,21 @@ import java.util.Optional;
 @Component
 @Slf4j
 public class Runner implements CommandLineRunner {
-    @Resource
-    private BaseDataRepository baseDataRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        final Optional<BaseData> byId = baseDataRepository.findById(1);
-        if (!byId.isPresent()){
-            long start = System.currentTimeMillis();
-            List<BaseData> list = new ArrayList<>();
-            for (int i = 0; i < 10000; i++) {
-                BaseData baseData = new BaseData();
-                baseData.setData("Hello - " + i);
-                list.add(baseData);
-            }
-            baseDataRepository.saveAll(list);
-            log.warn("{} : {} ms", "Total Cost", System.currentTimeMillis() - start);
+        long start = System.currentTimeMillis();
+        List<BaseData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            BaseData baseData = new BaseData();
+            baseData.setData("Hello - " + i);
+            entityManager.persist(baseData);
         }
+        entityManager.flush();
+        log.warn("{} : {} ms", "Total Cost", System.currentTimeMillis() - start);
     }
 }
