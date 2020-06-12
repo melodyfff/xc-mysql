a03bcc55433a48faf91526b9ad82cd5f822110f9
melodyfff <307208327@qq.com>
2020-06-12 17:07:38 +0800

    :smile: springboot-elasticsearch 通过filebeat推送数据到es，进行查询 更新心得体会

--- springboot-elasticsearch/src/main/java/com/xinchen/orm/elasticsearch/search/SearchController.java
+++ springboot-elasticsearch/src/main/java/com/xinchen/orm/elasticsearch/search/SearchController.java
@@ -20,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * 官网文档： https://docs.spring.io/spring-data/elasticsearch/docs/4.0.0.RELEASE/reference/html/#elasticsearch.scroll
  *
+ *  * 较低版本的在使用{@link ElasticsearchRestTemplate} 如：
+ *  * elasticsearchRestTemplate.query() , 可重写{@link ResultsExtractor},装填想要的结果 。 但是这里超过10000默认值时会报错
+ *  *
+ *  * elasticsearchRestTemplate.startScroll() 可重写{@link SearchResultMapper},封装想要的结果，这里没限制最大查询数
  * @author xinchen
  * @version 1.0
  * @date 01/06/2020 13:44
